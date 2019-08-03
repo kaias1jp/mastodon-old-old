ea162567c1cee58b32488c661c30f63bb33129d5
popn_ja <popn.ja@gmail.com>
2019-08-03 09:32:43 +0000

    modified:   instances_controller.rb

--- app/controllers/api/v1/instances_controller.rb
+++ app/controllers/api/v1/instances_controller.rb
@@ -2,10 +2,11 @@
 
 class Api::V1::InstancesController < Api::BaseController
   respond_to :json
+
   skip_before_action :set_cache_headers
 
   def show
     expires_in 3.minutes, public: true
-    render_with_cache json: {}, serializer: REST::InstanceSerializer
+    render_with_cache json: {}, serializer: REST::InstanceSerializer, root: 'instance'
   end
 end
